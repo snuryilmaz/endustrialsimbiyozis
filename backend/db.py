036d87f580abca19a47dc12497301c714051a84e
Sema Nur Yılmaz <114763034+snuryilmaz@users.noreply.github.com>
2025-10-28 23:37:14 +0300

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://username:password@localhost/simbiyoz"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "sqlite:///./test.db"  # Hızlı başlamak için SQLite önerilir!
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
