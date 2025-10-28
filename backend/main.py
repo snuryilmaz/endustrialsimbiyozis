3e6df5fda782d44f6bacf49d51a80a149ce31054
Sema Nur Yılmaz <114763034+snuryilmaz@users.noreply.github.com>
2025-10-28 22:48:11 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 from backend.routers import firma, atik
 
-app = FastAPI(
-    title="Endüstriyel Simbiyoz API",
-    description="Firmaların ürün alım/satım işlemlerini yöneten API",
-    version="1.0.0"
-)
+app = FastAPI(title="Endustriyel Simbiyoz Backend")
 
 app.include_router(firma.router)
 app.include_router(atik.router)
